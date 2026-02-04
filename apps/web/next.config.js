57505fad2be1752d5892323e8cf5c111a07da308
aidenkiefer <akief3@uic.edu>
2026-02-04 00:31:31 -0600

    prisma engines fix

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -6,25 +7,10 @@ const nextConfig = {
     // Monorepo: trace from repo root so Prisma engine in root node_modules is included in serverless bundle
     outputFileTracingRoot: path.join(__dirname, '../../'),
   },
-  // Don't externalize Prisma - let webpack bundle it
-  // This ensures the engines are included in the bundle
   webpack: (config, { isServer }) => {
     if (isServer) {
-      // Force Prisma to be bundled by webpack instead of externalized
-      // This makes Next.js include the engine binaries in the deployment
-      config.externals = config.externals.map((external) => {
-        if (typeof external === 'function') {
-          return async (context) => {
-            const result = await external(context)
-            // Don't externalize @prisma/client
-            if (result && (result.includes('@prisma/client') || result.includes('.prisma/client'))) {
-              return undefined
-            }
-            return result
-          }
-        }
-        return external
-      })
+      // Prisma monorepo workaround: copies engine binaries to the correct location
+      config.plugins = [...config.plugins, new PrismaPlugin()]
     }
 
     // Wagmi/connectors pull in optional deps that aren't used in browser build
